0f5807970372210ea563415f3493b7a964820c8a
Daniel Ramírez Morilla <daniel@xarblanca.es>
2022-05-19 19:00:26 +0200

    Initialize UI

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,8 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     private int points = 0;
 
+    [SerializeField]
+    private TMPro.TextMeshProUGUI ebis;
+
     public void AddPoints(int points)
     {
         this.points += points;
+        ebis.text = this.points.ToString();
     }
 }
